d6f801a8c4c1e2ae01f7ed75390346743183546d
Lester0504 <137714720+Lester0504@users.noreply.github.com>
2023-07-02 09:15:24 +0800

    mian.sh

--- script/mian.sh
+++ script/mian.sh
@@ -16,6 +16,30 @@ curl -o i-4.txt https://cdn.jsdelivr.net/gh/privacy-protection-tools/anti-AD@mas
 curl -o i-4.txt https://easylist-downloads.adblockplus.org/antiadblockfilters.txt
 curl -o i-4.txt https://cdn.jsdelivr.net/gh/damengzhu/banad/jiekouAD.txt
 curl -o i-4.txt https://www.i-dont-care-about-cookies.eu/abp/
+curl -o i-4.txt https://cdn.jsdelivr.net/gh/crazy-max/WindowsSpyBlocker/data/hosts/spy.txt
+curl -o i-4.txt https://cdn.jsdelivr.net/gh/o0HalfLife0o/list@master/ad-pc.txt
+curl -o i-4.txt https://cdn.jsdelivr.net/gh/VeleSila/yhosts/hosts.txt
+curl -o i-4.txt https://gitlab.com/quidsup/notrack-blocklists/-/raw/master/trackers.list
+curl -o i-4.txt https://raw.githubusercontent.com/AdguardTeam/cname-trackers/master/combined_disguised_trackers.txt、
+curl -o i-4.txt https://raw.githubusercontent.com/Crystal-RainSlide/AdditionalFiltersCN/master/CN.txt
+curl -o i-4.txt https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Formats/GoodbyeAds-AdBlock-Filter.txt
+curl -o i-4.txt https://www.trli.club/ad-hosts/ad-hosts-sqlist/ad-adguardhome-dnstype.txt
+curl -o i-4.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/dns.txt
+curl -o i-4.txt https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/AdGuard/Advertising/Advertising.txt
+curl -o i-4.txt https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockfilters.txt
+curl -o i-4.txt https://raw.iqiq.io/ilxp/koolproxy/master/rules/koolproxy.txt
+curl -o i-4.txt https://raw.iqiq.io/ilxp/koolproxy/master/rules/daily.txt
+curl -o i-4.txt https://raw.iqiq.io/ilxp/koolproxy/master/rules/adgk.txt
+curl -o i-4.txt https://raw.iqiq.io/ilxp/koolproxy/master/rules/steven.txt
+curl -o i-4.txt https://ublockorigin.pages.dev/filters/badware.txt
+curl -o i-4.txt https://gitcdn.link/cdn/uBlockOrigin/uAssetsCDN/main/filters/privacy.txt
+curl -o i-4.txt https://ublockorigin.github.io/uAssets/filters/quick-fixes.txt
+curl -o i-4.txt https://cdn.statically.io/gh/uBlockOrigin/uAssetsCDN/main/filters/resource-abuse.txt
+curl -o i-4.txt https://gitcdn.link/cdn/uBlockOrigin/uAssetsCDN/main/filters/unbreak.txt
+curl -o i-4.txt https://pgl.yoyo.org/adservers/serverlist.php?hostformat=adblockplus&showintro=1&mimetype=plaintext
+curl -o i-4.txt https://github.com/Potterli20/file/releases/download/ad-youtube-hosts/ad-youtube-adguardhome.txt
+curl -o i-4.txt https://filters.adtidy.org/extension/ublock/filters/11.txt
+curl -o i-4.txt https://github.com/Potterli20/file/releases/download/ad-hosts-pro/ad-adguardhome-dnstype.txt
 
 # 合并规则并去除重复项
 cat i*.txt > i-mergd.txt
